06ca3a987a81fc7714c1cc8aed640875461a6089
hyemimi <hyemi7375@gmail.com>
2023-08-10 15:00:29 +0900

    업데이트 여부 창 띄우기

--- src/main/index.ts
+++ src/main/index.ts
@@ -4,9 +4,11 @@ import { electronApp, optimizer, is } from '@electron-toolkit/utils';
 import icon from '../../resources/icon.png?asset';
 import { autoUpdater } from 'electron-updater';
 import { Menu } from 'electron';
+import ProgressBar from 'electron-progressbar';
 
 let mainWindow;
 const isMac = process.platform === 'darwin';
+autoUpdater.autoDownload = false;
 
 const createWindow = () => {
   // Create the browser window.
@@ -60,42 +62,106 @@ app.whenReady().then(() => {
 
   createWindow();
 
-  autoUpdater.checkForUpdatesAndNotify();// 업데이트 체크
+  autoUpdater.checkForUpdates(); // autoUpdater에서 업데이트를 감지함
 
-  autoUpdater.on('checking-for-update', () => {
-    console.log('Checking for update...');
+  autoUpdater.on('update-available', () => {
+    dialog
+      .showMessageBox({
+        type: 'info',
+        title: 'Update available',
+        message:
+          '새로운 버전으로 업데이트가 가능합니다. 업데이트 하시겠습니까?',
+        buttons: ['Update', 'Later']
+      })
+      .then((result) => {
+        const buttonIndex = result.response;
+
+        if (buttonIndex === 0) autoUpdater.downloadUpdate();
+      });
   });
 
-  autoUpdater.on('update-available', (info) => {
-    console.log('Update available:', info);
+  autoUpdater.on('download-progress', (progressObj) => { console.log('다운로드 진행중'); });
+  /*  autoUpdater.on('download-progress', (progressObj) => {
+    progressBar
+      .on('completed', function() {
+        progressBar.value = progressObj.percent;
+        progressBar.detail = 'Task completed. Exiting...';
+      })
+      .on('aborted', function(value) {
+      })
+      .on('progress', function(value) {
+        progressBar.value = progressObj.percent;
+        progressBar.detail = `Downloading...  ${(progressObj.bytesPerSecond / 1000).toFixed(2)} 
+        KB/s (${(progressObj.transferred / 1000000).toFixed(2)} MB / ${(progressObj.total / 1000000).toFixed(2)} MB)`;
+      });
+    /* progressBar.value = progressObj.percent;
+    progressBar.detail = `Downloading...  ${(progressObj.bytesPerSecond / 1000).toFixed(2)} 
+    KB/s (${(progressObj.transferred / 1000000).toFixed(2)} MB / ${(progressObj.total / 1000000).toFixed(2)} MB)`; 
   });
-
-  autoUpdater.on('update-not-available', () => {
-    console.log('No update available.');
-  });
-
-  autoUpdater.on('error', (err) => {
-    console.error('Error in auto-updater:', err);
-  });
-
-  autoUpdater.on('download-progress', (progressObj) => {
-    console.log(`Download speed: ${progressObj.bytesPerSecond}`);
-    console.log(`Downloaded ${progressObj.percent}%`);
-    console.log(`Total downloaded: ${progressObj.transferred}/${progressObj.total}`);
-  });
+  */
 
   autoUpdater.on('update-downloaded', (info) => {
+
+    //progressBar.setCompleted();
+    //progressBar.close();
+    
+    /*  const selectBox = {
+      type: 'info',
+      defaultId: 0,
+      title: 'electron-updater',
+      message: '설치 후 재시작합니다'
+    };
+    const btnIndex = dialog.showMessageBoxSync(mainWindow, selectBox);
+    if (btnIndex === 0) { */
+    autoUpdater.quitAndInstall();
+    
+  });
+
+  /*  autoUpdater.on('error', (err) => {
+    console.error('Error in auto-updater:', err);
+  }); */
+
+  /*   const Box = {
+      type: 'info',
+      title: 'electron-updater',
+      message: '다운로드가 완료되었습니다!'
+    };
+    dialog.showMessageBoxSync(mainWindow, Box);// 업데이트를 설치하고 재시작한다 update-downloaded가 끝난 뒤 발생
+  }); */
+
+  /* autoUpdater.on('download-progress', (progressObj) => {
+    let log_message;
+    log_message = log_message + ` - Downloaded ${progressObj.percent}%`;
+    log_message = log_message + `Total downloaded: ${progressObj.transferred}/${progressObj.total}`;
+    
+    const progress = {
+      type: ''
+    }
+  }); */ // 커스텀 가능한지?
+
+  /* autoUpdater.on('update-downloaded', (info) => {
     console.log('Update downloaded:', info);
 
-    // 업데이트 설치 여부를 묻는 다이얼로그 또는 자동 업데이트를 선택하여 업데이트 수행
-    autoUpdater.quitAndInstall();
-  });
-  autoUpdater.checkForUpdatesAndNotify();
+    const selectBox = {
+      type: 'question',
+      buttons: ['업데이트','취소'],
+      defaultId: 0,
+      title: 'electron-updater',
+      message: '새로운 버전으로 업데이트 할 수 있습니다. 업데이트 하시겠습니까?'
+    };
+    const btnIndex = dialog.showMessageBoxSync(mainWindow, selectBox);
+
+    if (btnIndex === 0) {
+      autoUpdater.downloadUpdate();
+    }
+
+    mainWindow?.webContents.send('message','두둥~');
+  }); */
 
   app.on('activate', () => {
-   
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
+
 });
 
 app.on('window-all-closed', () => {
